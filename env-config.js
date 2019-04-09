fe59492097d95afe2fc8658e9be646506ec13a42
Muhammad Ahmed Saeedi <ahmed.saeedi@drtechpk.com>
2019-04-09 14:55:22 +0500

    Update env-config.js

--- env-config.js
+++ env-config.js
@@ -2,6 +2,6 @@ const prod = process.env.NODE_ENV === 'production';
 
 module.exports = {
   'process.env.BASE_URL': prod ? 'https://mahmedsaeedi.herokuapp.com' : 'http://localhost:3000',
-  'process.env.NAMESPACE': prod ? 'https://mahmedsaeedi.herokuapp.com':'http://localhost:3000',
+  'process.env.NAMESPACE': 'https://mahmedsaeedi.herokuapp.com',
   'process.env.CLIENT_ID': 'zri0QM14UWsltOdTpWMSGSEyOiQFiJA2'
 }
